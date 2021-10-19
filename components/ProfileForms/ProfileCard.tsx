35fae0c3c5a35125dbc76a96e14d5e6da779f9f6
Sparsh Agarwal <sparshaga25@gmail.com>
2021-10-19 21:18:00 +0530

    Created a form for Profile Cart (UI Only)

---
+++ components/ProfileForms/ProfileCard.tsx
@@ -0,0 +1,266 @@
+import React from 'react';
+
+const ProfileCard: React.FC = () => {
+    return (
+        <>
+            <div>
+                <div className="md:grid md:grid-cols-3 md:gap-6">
+                    <div className="md:col-span-1">
+                        <div className="px-4 sm:px-0">
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">
+                                Profile
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-600">
+                                This information will be displayed publicly so
+                                be careful what you share.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-5 md:mt-0 md:col-span-2">
+                        <form action="#" method="POST">
+                            <div className="shadow sm:rounded-md sm:overflow-hidden">
+                                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                                    <div className="grid grid-cols-6 gap-6">
+                                        {/* Profile Photo Start */}
+
+                                        <div className="col-span-6">
+                                            <label className="block text-sm font-medium text-gray-700">
+                                                Profile Photo
+                                            </label>
+                                            <div className="mt-1 flex items-center">
+                                                <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                                                    <svg
+                                                        className="h-full w-full text-gray-300"
+                                                        fill="currentColor"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                                                    </svg>
+                                                </span>
+                                                <button
+                                                    type="button"
+                                                    className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                >
+                                                    Change
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                        {/* Profile Photo End */}
+
+                                        {/* Name Start */}
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="first-name"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Full Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="first-name"
+                                                id="first-name"
+                                                autoComplete="given-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div>
+                                        {/* Name End */}
+
+                                        {/* Position Start */}
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="last-name"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Position
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div>
+
+                                        {/* Position End */}
+
+                                        {/* About Start */}
+
+                                        <div className="col-span-6">
+                                            <label
+                                                htmlFor="about"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                About
+                                            </label>
+                                            <div className="mt-1">
+                                                <textarea
+                                                    id="about"
+                                                    name="about"
+                                                    rows={3}
+                                                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                                    placeholder="you@example.com"
+                                                    defaultValue={''}
+                                                />
+                                            </div>
+                                            <p className="mt-2 text-sm text-gray-500">
+                                                Brief description for your
+                                                profile. URLs are hyperlinked.
+                                            </p>
+                                        </div>
+
+                                        {/* About End */}
+
+                                        {/* Status Start */}
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="status"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Status
+                                            </label>
+                                            <select
+                                                id="status"
+                                                name="status"
+                                                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            >
+                                                <option>Student</option>
+                                                <option>Graduation</option>
+                                            </select>
+                                        </div>
+
+                                        {/* Status End */}
+
+                                        {/* Graduation Date Start */}
+
+                                        <div className=" col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="graduation-date"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Graduation Date
+                                            </label>
+
+                                            <div className=" grid grid-cols-2 gap-2 col-span-6 sm:col-span-3">
+                                                <select
+                                                    id="graduation-month"
+                                                    name="graduation-month"
+                                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                >
+                                                    {/* TODO: Check whether this System Works */}
+                                                    <option selected disabled>
+                                                        Month
+                                                    </option>
+                                                    <option>January</option>
+                                                    <option>February</option>
+                                                    <option>March</option>
+                                                    <option>April</option>
+                                                    <option>May</option>
+                                                    <option>June</option>
+                                                    <option>July</option>
+                                                    <option>August</option>
+                                                    <option>September</option>
+                                                    <option>October</option>
+                                                    <option>November</option>
+                                                    <option>December</option>
+                                                </select>
+
+                                                <select
+                                                    id="graduation-year"
+                                                    name="graduation-year"
+                                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                >
+                                                    {/* TODO: Check whether this System Works */}
+                                                    <option selected disabled>
+                                                        Year
+                                                    </option>
+                                                    <option>2012</option>
+                                                    <option>2013</option>
+                                                    <option>2014</option>
+                                                    <option>2015</option>
+                                                    <option>2016</option>
+                                                    <option>2017</option>
+                                                    <option>2018</option>
+                                                    <option>2019</option>
+                                                    <option>2020</option>
+                                                    <option>2021</option>
+                                                    <option>2022</option>
+                                                    <option>2023</option>
+                                                    <option>2024</option>
+                                                    <option>2025</option>
+                                                    <option>2026</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        {/* Graduation Date End */}
+
+                                        {/* Degree Start */}
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="last-name"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Degree
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div>
+
+                                        {/* Degree End */}
+
+                                        {/* Branch Start */}
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="last-name"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Branch
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                        </div>
+
+                                        {/* Branch End */}
+                                    </div>
+                                </div>
+                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button
+                                        type="submit"
+                                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div className="hidden sm:block" aria-hidden="true">
+                <div className="py-5">
+                    <div className="border-t border-gray-200" />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProfileCard;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,61 +1,20 @@
-import {
-    NextPage,
-    GetServerSideProps,
-    InferGetServerSidePropsType,
-} from 'next';
+import { NextPage } from 'next';
 import { Navbar } from '@components/Navigation';
-import {
-    ProfileCard,
-    AboutSection,
-    ExpereinceSection,
-    EducationSection,
-    CertificationSection,
-    SkillsSection,
-} from '@components/Profile';
-import { StudentProfileType } from '@utilities/profileDataTypes';
-import { getUserDataByUsername } from '@utilities/dataFunctions';
+import ProfileCard from '@components/ProfileForms/ProfileCard';
 
-const Profile: NextPage<
-    InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ data }) => {
+const Profile: NextPage = () => {
     return (
         <>
             <Navbar />
             <main className="bg-gray-100">
                 <div className="container mx-auto p-5">
-                    <div className="md:flex no-wrap md:-mx-2">
-                        <div className="w-full md:w-3/12 md:mx-2">
-                            <ProfileCard data={data.profile} />
-                        </div>
-
-                        <div className="w-full md:w-9/12 mx-2 grid gap-3">
-                            <AboutSection data={data.about} />
-                            <SkillsSection data={data.skills} />
-                            <ExpereinceSection data={data.expereince} />
-                            <EducationSection data={data.education} />
-                            <CertificationSection data={data.certification} />
-                        </div>
+                    <div className=" no-wrap md:-mx-2">
+                        <ProfileCard />
                     </div>
                 </div>
             </main>
         </>
     );
-};
-
-type Data = {
-    data: StudentProfileType;
-};
-
-export const getServerSideProps: GetServerSideProps<Data> = async (ctx) => {
-    const username: string = 'akathecoder';
-
-    const data = await getUserDataByUsername(username);
-
-    return {
-        props: {
-            data: data,
-        },
-    };
 };
 
 export default Profile;
