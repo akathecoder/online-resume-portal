d23859614276ac96ebf8d6b6af52d2986992707e
Sparsh Agarwal <sparshaga25@gmail.com>
2021-11-09 11:04:30 +0530

    debug

--- pages/api/createUserOnRegistration.ts
+++ pages/api/createUserOnRegistration.ts
@@ -6,14 +6,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if (req.body.username && req.body.apiKey) {
             if (req.body.apiKey === process.env.AUTH0_CREATE_USER_API_KEY) {
                 await createUser(req.body.username);
+                console.log(req.body.event);
                 res.status(200).send('User Created');
             } else {
+                console.log('API KEY ERROR');
                 res.status(401).send('API KEY ERROR');
             }
         } else {
             res.status(400).json({
                 message: 'Username not provided',
             });
+            console.log('username not provided');
         }
     } else {
         res.status(400).json({
