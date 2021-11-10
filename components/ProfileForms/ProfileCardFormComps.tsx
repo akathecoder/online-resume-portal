fa0823453f2e9ca78fa82c55cc1a92d554bdfc5f
Sparsh Agarwal <sparshaga25@gmail.com>
2021-11-10 19:15:54 +0530

    bug fix

--- components/ProfileForms/ProfileCardFormComps.tsx
+++ components/ProfileForms/ProfileCardFormComps.tsx
@@ -201,7 +201,7 @@ export const GraduationDate: React.FC<ValueProps> = ({
                     className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 >
                     {/* TODO: Check whether this System Works */}
-                    <option selected disabled>
+                    <option selected disabled hidden>
                         Month
                     </option>
                     <option>January</option>
@@ -228,7 +228,7 @@ export const GraduationDate: React.FC<ValueProps> = ({
                     className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 >
                     {/* TODO: Check whether this System Works */}
-                    <option selected disabled>
+                    <option selected disabled hidden>
                         Year
                     </option>
                     <option>2012</option>
