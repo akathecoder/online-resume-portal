0cd1b6d1b46e3e81fb0ee35937e8a4ee8d091149
Sparsh Agarwal <sparshaga25@gmail.com>
2021-12-06 21:10:07 +0530

    debug status box

--- components/ProfileForms/ProfileCardFormComps.tsx
+++ components/ProfileForms/ProfileCardFormComps.tsx
@@ -158,8 +158,8 @@ export const Status: React.FC<ValueProps> = ({
                 }}
                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
-                <option>Student</option>
-                <option>Graduation</option>
+                <option value="student">Student</option>
+                <option value="graduation">Graduation</option>
             </select>
         </div>
     );
